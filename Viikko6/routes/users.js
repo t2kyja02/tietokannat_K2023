c7e8c1f5b5df35a622c199d87b38a512157d71fc
t2kyja02 <kynkky02@hotmail.com>
2023-02-22 23:43:18 +0200

    Viikon 6 tehtävät

---
+++ Viikko6/routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/toka', function(request, response) {
+	response.send('Olen toka reitti');
+	console.log('Toka');
+});
+
+router.get('/kolmas/:name', function(request, response) {
+	response.send('Heippa '+request.params.name);
+	console.log(request.params.name);
+});
+
+router.get('/neljas/:fname/:lname', function(request, response) {
+	response.send('Heippa '+request.params.fname+' '+request.params.lname);
+	console.log(request.params.fname+' '+request.params.lname);
+});
+
+router.post('/', function(request, response) {
+	response.send(request.body.fname+' '+request.body.lname);
+	console.log(request.body.fname+' '+request.body.lname)
+});
+
+module.exports = router;
